8d686b28e26b6bc0ec1e21e1184e09167a6da6dd
Guro Bokum <jiojiajiu@gmail.com>
2015-03-29 23:06:28 +0700

    Problem-9

---
+++ Problem-9/Main.hs
@@ -0,0 +1,16 @@
+--Special Pythagorean triplet
+
+find [] = []
+find (x:xs) =
+    findB x b
+    where
+        b = [x..998]
+        findB a1 [] = find xs
+        findB a1 (b1:b)
+            | a1 + b1 + c == 1000 && (a1^2 + b1^2 == c^2) = [a1, b1, c]
+            | otherwise = findB a1 b
+            where
+                c = 1000 - a1 - b1
+
+main = do
+    print $ find [1..998]
