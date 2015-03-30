92d4df31afb0ef8b67ecb94f6a8418787097cb80
Guro Bokum <jiojiajiu@gmail.com>
2015-03-30 11:42:34 +0700

    refactoring: constant, foldl1 -> product

--- Problem-9/Main.hs
+++ Problem-9/Main.hs
@@ -1,16 +1,17 @@
 --Special Pythagorean triplet
+number = 1000
 
 find [] = []
 find (x:xs) =
     findB x b
     where
-        b = [x..998]
+        b = [x..(number - 2)]
         findB a1 [] = find xs
         findB a1 (b1:b)
-            | a1 + b1 + c == 1000 && (a1^2 + b1^2 == c^2) = [a1, b1, c]
+            | a1^2 + b1^2 == c^2 = [a1, b1, c]
             | otherwise = findB a1 b
             where
-                c = 1000 - a1 - b1
+                c = number - a1 - b1
 
 main = do
-    print $ foldl1 (*) $ find [1..998]
+    print $ product $ find [1..(number - 2)]
